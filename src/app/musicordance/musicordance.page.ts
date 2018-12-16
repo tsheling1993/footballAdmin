180e07bb2ade9c3a93753b2b72067efa55518139
SonamDorji1995 <sonamfechurdorji@gmail.com>
2018-12-16 09:57:07 +0600

    music

--- src/app/musicordance/musicordance.page.ts
+++ src/app/musicordance/musicordance.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AlertController, NavController } from '@ionic/angular';
+import { DatePicker } from '@ionic-native/date-picker/ngx';
 
 @Component({
   selector: 'app-musicordance',
@@ -6,8 +9,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./musicordance.page.scss'],
 })
 export class MusicordancePage implements OnInit {
-
-  constructor() { }
+  rTitle : any;
+  rVenue : any;
+  rTime : any;
+  rDate : any;
+  rContact : any;
+  rLink : any;
+  date : any;
+  rData:any[]=[];
+  constructor(
+    private fs : AngularFirestore,
+    private altCtl : AlertController,
+    private navCtl : NavController,
+    private datePicker: DatePicker
+  ) { 
+    //for retriving the entertainment data from the database
+    this.fs.collection('/t_entertainment').get().subscribe(res=>
+      {
+        res.forEach((doc:any)=>
+        {
+          this.rData.push({
+            title : doc.data().title,
+            venue : doc.data().venue,
+            time : doc.data().time,
+            date : doc.data().date,
+            contact : doc.data().contact,
+            link : doc.data().link,
+          })
+        })
+      })
+      console.log(this.rData);
+  }
 
   ngOnInit() {
   }
